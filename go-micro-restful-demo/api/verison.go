2f95956fe0bd8b38fa06403f1f72ebbaea174083
yuepan <yuepan>
2018-09-10 11:33:00 +0800

    123

---
+++ go-micro-restful-demo/api/verison.go
@@ -0,0 +1,35 @@
+package api
+
+import (
+	mVersion "github.com/py19912214/golang-playground/go-micro-restful-demo/proto/go"
+	"golang.org/x/net/context"
+	"fmt"
+)
+
+type Version struct {
+	VersionHandler
+}
+
+func (v *Version) Version(ctx context.Context, req *mVersion.Request, resp *mVersion.Response) error {
+	//*req = mVersion.Request{
+	//	Name: "youName",
+	//}
+	*resp = mVersion.Response{
+		Vc:  10,
+		Vn:  "0.0.010",
+		Msg: fmt.Sprintf("micro_demo you send name => %v", req.Name),
+	}
+	return nil
+}
+
+func (v *Version) GetVersion(ctx context.Context, req *mVersion.Request, resp *mVersion.Response) error {
+	*resp = mVersion.Response{
+		Vc:  100,
+		Vn:  "0.0.100",
+		Msg: fmt.Sprintf("you send name => %v", req.Name),
+	}
+	return nil
+}
+
+type VersionHandler interface {
+}
---
+++ go-micro-restful-demo/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"time"
+	"log"
+	"github.com/micro/go-micro"
+    "github.com/py19912214/golang-playground/go-micro-restful-demo/api"
+)
+
+func main() {
+	service := micro.NewService(
+		micro.Name("go.micro.restful.demo"),
+		micro.Version("last"),
+		micro.RegisterTTL(time.Second*30),
+		micro.RegisterInterval(time.Second*10),
+		micro.Metadata(map[string]string{
+			"type": "RESTful demo",
+		}),
+	)
+
+	// optionally setup command line usage
+	service.Init()
+
+	server := service.Server()
+	// Register Handlers
+	server.Handle(server.NewHandler(&api.Version{}))
+
+	// Run server
+	if err := service.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
